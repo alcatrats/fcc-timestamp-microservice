de4b253d1bc4d8ae56a1c99aa9ab7118006bd85b
alcatrats <alex.evans.d@gmail.com>
2016-03-11 03:21:27 +0000

    Good to go

--- app/controllers/dateHandler.server.js
+++ app/controllers/dateHandler.server.js
@@ -1,24 +1,25 @@
 'user strict';
 
-var months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
 function dateHandler() {
-    var date;
-    var unixTime;
-    var natTime;
-    var rtnVal;
-    
+
     this.getDate = function(req, res) {
+        var date;
+        var unixTime = null;
+        var natTime = null;
+        var rtnVal;
+
         if(/^\d+$/.test(req.params.date)) {
             unixTime = parseInt(req.params.date, 10);
             date = new Date(unixTime * 1000);
             natTime = months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
-        } else {
-            var dateArray = req.params.date.split(" ");
 
-            // date = new Date(req.params.date);
+        } else if(validDate(req.params.date)) {
+            var dateArray = req.params.date.split(" ");
             unixTime = Date.parse(req.params.date) / 1000;
             natTime = dateArray[0] + " " + dateArray[1] + " " + dateArray[2];
+
         }
         
         rtnVal = {"unix": unixTime, "natural": natTime};
@@ -26,6 +27,19 @@ function dateHandler() {
         return res.json(rtnVal);
 
     };
+}
+
+function validDate(dateChk) {
+    var dt = new Date(dateChk);
+    if(Object.prototype.toString.call(dt) === "[object Date]") {
+        if(isNaN(dt.getTime())) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 }
 
 
